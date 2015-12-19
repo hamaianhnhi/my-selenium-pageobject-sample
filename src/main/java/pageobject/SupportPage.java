6bbf7b29dc6223a7ac228adce29597cd05c9bc35
lazhcm10185 <tuananhhcmus@gmail.com>
2015-12-19 16:37:35 +0700

    init project

---
+++ src/main/java/pageobject/SupportPage.java
@@ -0,0 +1,7 @@
+package pageobject;
+
+/**
+ * Created by lazhcm10185 on 12/19/15.
+ */
+public class SupportPage {
+}
---
+++ src/test/java/HomePageTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pageobject.HomePage;
+
+
+public class HomePageTest {
+
+    @Test
+    public void testThatUserShouldBeAbleToClickOnProjectsButton(){
+        WebDriver driver = new FirefoxDriver();
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnProjectsButton();
+        homePage.clickOnDownloadButton();
+        homePage.clickOnProjectsButton();
+        homePage.clickOnDownloadButton();
+        homePage.clickOnProjectsButton();
+        homePage.clickOnDownloadButton();
+        homePage.clickOnProjectsButton();
+        homePage.clickOnDownloadButton();
+        driver.quit();
+    }
+
+    @Test
+    public void testThatUserShouldBeAbleToClickDownloadButton(){
+        WebDriver driver = new FirefoxDriver();
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnDownloadButton();
+        homePage.clickOnProjectsButton();
+        homePage.clickOnDownloadButton();
+        homePage.clickOnProjectsButton();
+        homePage.clickOnDownloadButton();
+        homePage.clickOnProjectsButton();
+        homePage.clickOnDownloadButton();
+        homePage.clickOnProjectsButton();
+        driver.quit();
+    }
+}

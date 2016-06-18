3f1981881e72ed02decca4e5af166a4ab304c13e
lazhcm10185 <tuananhhcmus@gmail.com>
2016-06-18 19:26:55 +0700

    [dev-1-refactor] add TestConfiguration.java

--- src/test/java/helpers/TestConfiguration.java
+++ src/test/java/helpers/TestConfiguration.java
@@ -1,4 +1,9 @@
 package helpers;
 
 public class TestConfiguration {
+
+    public static final String BROWSER_NAME = System.getProperty("browser-name");
+    public static final String TIME_OUT = System.getProperty("timeout");
+    public static final String CHROME_DRIVER_PATH = System.getProperty("chrome-driver-path");
+
 }

8746226cf4fd75a63c7398537f6a762efd95a69e
lazhcm10185 <tuananhhcmus@gmail.com>
2016-06-18 19:31:37 +0700

    [dev-1-refactor] some changes in HomePageTest.java

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -1,23 +1,25 @@
 package tests;
 
+import helpers.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import pageobject.HomePage;
 
 public class HomePageTest {
 
     @Test
-    public void testThatUserShouldBeAbleToClickOnProjectsButton(){
-        WebDriver webDriver = new FirefoxDriver();
+    public void test1(){
+        System.out.println(">>>>test1");
+        WebDriver webDriver = WebDriverFactory.configWebDriver();
         HomePage  homePage = new HomePage(webDriver);
         homePage.navigationDownLoadButton().click();
         webDriver.quit();
     }
 
     @Test
-    public void abc(){
-        WebDriver webDriver = new FirefoxDriver();
+    public void test2(){
+        System.out.println(">>>>test2");
+        WebDriver webDriver = WebDriverFactory.configWebDriver();
         HomePage  homePage = new HomePage(webDriver);
         homePage.navigationDownLoadButton().click();
         webDriver.quit();
